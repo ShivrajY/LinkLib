37263492aef295ef0d94d220e95c1ec71aaf1e4a
shiv <suprememind@live.com>
2022-05-18 16:53:51 +0530

    .

---
+++ ConsoleTest/Program.fs
@@ -0,0 +1,12 @@
+﻿open LinkLib
+open System
+
+let url = "", "https://eclipse2024.org"
+
+let agent = WebCrawler.Webcrawler("https://eclipse2024.org")
+
+agent.Post url
+
+printfn "Started"
+
+Console.ReadLine() |> ignore
